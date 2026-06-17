c4fea1265ab3050c2a67c77343bfaaf36c9af146
Emin <emin.erturk@icloud.com>
2026-06-17 14:50:52 +0300

    fix(e2e): run container tests with container uid gid mapping
    map host uid/gid inside container before running e2e tests so writable
    paths stay accessible when the container user is not 1000

--- e2e/arch-container/run.sh
+++ e2e/arch-container/run.sh
@@ -31,12 +31,33 @@ fi
 
 "$runtime" run --rm \
 	"$@" \
-	--user "$uid:$gid" \
 	-e HOME=/tmp/konfi-home \
 	-e GOCACHE=/tmp/konfi-go-cache \
 	-e GOMODCACHE=/tmp/konfi-go-mod-cache \
+	-e KONFI_CONTAINER_E2E_UID="$uid" \
+	-e KONFI_CONTAINER_E2E_GID="$gid" \
 	-e XDG_CONFIG_HOME=/tmp/konfi-xdg \
 	-v "$root:/work:ro" \
 	-w /work/src \
 	"$image" \
-	sh -lc 'mkdir -p "$HOME" "$GOCACHE" "$GOMODCACHE" "$XDG_CONFIG_HOME" && go test -count=1 -tags=container_e2e -run TestContainerE2E ./konfables'
+	sh -lc '
+set -eu
+
+uid=${KONFI_CONTAINER_E2E_UID:-1000}
+gid=${KONFI_CONTAINER_E2E_GID:-1000}
+
+group_name=$(getent group "$gid" | cut -d: -f1 || true)
+if [ -z "$group_name" ]; then
+	group_name=konfi
+	groupadd -g "$gid" "$group_name"
+fi
+if ! getent passwd "$uid" >/dev/null; then
+	useradd -u "$uid" -g "$gid" -d "$HOME" -s /bin/sh konfi
+fi
+
+mkdir -p "$HOME" "$GOCACHE" "$GOMODCACHE" "$XDG_CONFIG_HOME"
+chown -R "$uid:$gid" "$HOME" "$GOCACHE" "$GOMODCACHE" "$XDG_CONFIG_HOME"
+
+exec setpriv --reuid "$uid" --regid "$gid" --init-groups \
+	sh -lc "go test -count=1 -tags=container_e2e -run TestContainerE2E ./konfables"
+'
